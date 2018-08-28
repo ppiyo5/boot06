bb926022483396c6a8efa5f478b980deccdcc4c5
Fine <Fine@DESKTOP-1QB7RCK>
2018-08-28 10:16:43 +0900

    페이징

--- src/main/java/org/zerock/vo/PageVO.java
+++ src/main/java/org/zerock/vo/PageVO.java
@@ -6,6 +6,10 @@ import org.springframework.data.domain.Sort;
 
 public class PageVO {
 	
+	/*브라우저에서 전달되는 값은 페이지 번호(page)와 게시물의 수(size)만을 받도록 설계.
+	이때에도 일정 이상의 값이 들어올 수 없도록 제약을 둔다.
+	이후 정렬 방향이나, 정렬 기준이 되는 속성은 컨트로럴에서 지정한다.*/
+	
 	private static final int DEFAULT_SIZE = 10;
 	private static final int DEFAULT_MAX_SIZE = 50;
 	
@@ -33,6 +37,7 @@ public class PageVO {
 		this.size = size < DEFAULT_SIZE || size > DEFAULT_MAX_SIZE ? DEFAULT_SIZE : size;
 	}
 	
+	//String...(var) args를 사용하면 배열 생성을 생략 할 수 있다.
 	public Pageable makePageable(int direction, String...props) {
 		
 		Sort.Direction dir = direction == 0? Sort.Direction.DESC : Sort.Direction.ASC;
